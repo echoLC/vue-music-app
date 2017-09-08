597e4f7c6d77c264f94290c9dd11aa2cb9ed6ab0
echoLC <651644100@qq.com>
2017-09-08 16:44:12 +0800

    finish the baes function create

--- src/common/js/dom.js
+++ src/common/js/dom.js
@@ -23,3 +23,36 @@ export function getData(el, name, val) {
   }
   return el.getAttribute(name)
 }
+
+let elementStyle = document.createElement('div').style
+
+let vendor = (() => {
+  let transformNames = {
+    webkit: 'webkitTransform',
+    Moz: 'MozTransform',
+    O: 'OTransform',
+    ms: 'msTransform',
+    standard: 'transform'
+  }
+
+  for (let key in transformNames) {
+    if (elementStyle[transformNames[key]] !== undefined) {
+      return key
+    }
+  }
+
+  return false
+})()
+
+export function prefixStyle(styleName) {
+  if (vendor === false) {
+    return
+  }
+
+  if (vendor === 'standard') {
+    return styleName
+  }
+
+  return vendor + styleName.charAt(0).toUpperCase() + styleName.substr(1)
+}
+
