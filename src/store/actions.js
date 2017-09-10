5e73683b12593979dc576f78d03d0382658e930b
echoLC <651644100@qq.com>
2017-09-10 16:58:55 +0800

    fix the music list random play function

--- src/store/actions.js
+++ src/store/actions.js
@@ -2,12 +2,35 @@
  * Created by echoLC on 2017/9/8.
  */
 import * as types from './mutation-types'
+import {playMode} from 'common/js/config'
+import {shuffle} from 'common/js/util'
+
+function findIndex(song, list) {
+  return list.findIndex((item) => {
+    return item.id === song.id
+  })
+}
 
 export function selectPlay({commit, state}, {list, index}) {
   commit(types.SET_SEQUENCE_LIST, list)
-  commit(types.SET_PLAYLIST, list)
+  if (state.mode === playMode.random) {
+    let randomList = shuffle(list)
+    commit(types.SET_PLAYLIST, randomList)
+    index = findIndex(list[index], randomList)
+  } else {
+    commit(types.SET_PLAYLIST, list)
+  }
   commit(types.SET_CURRENT_INDEX, index)
   commit(types.SET_FULL_SCREEN, true)
   commit(types.SET_PLAYING_STATE, true)
 }
 
+export function randomPlay({commit}, {list}) {
+  commit(types.SET_PLAY_MODE, playMode.random)
+  commit(types.SET_SEQUENCE_LIST, list)
+  let randomList = shuffle(list)
+  commit(types.SET_PLAYLIST, randomList)
+  commit(types.SET_CURRENT_INDEX, 0)
+  commit(types.SET_FULL_SCREEN, true)
+  commit(types.SET_PLAYING_STATE, true)
+}
