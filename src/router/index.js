72f3345cefc97a633ef98463744992543ae14537
罗超 <luochao@mingbikes.com>
2017-09-11 23:31:27 +0800

    add the rank and top-list page

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,7 @@ import Search from 'components/Search/Search'
 import Singer from 'components/Singer/Singer'
 import singerDetail from 'components/singer-detail/singer-detail'
 import Disc from 'components/disc/disc'
+import TopList from 'components/top-list/top-list'
 
 Vue.use(Router)
 
@@ -28,7 +29,13 @@ export default new Router({
     },
     {
       path: '/rank',
-      component: Rank
+      component: Rank,
+      children: [
+        {
+          path: ':id',
+          component: TopList
+        }
+      ]
     },
     {
       path: '/search',
