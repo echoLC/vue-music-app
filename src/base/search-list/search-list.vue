3c5ac1e56e0204c11afaa363de331a8c55ea971a
echoLC <651644100@qq.com>
2017-09-16 14:38:20 +0800

    fix the item key

--- src/base/search-list/search-list.vue
+++ src/base/search-list/search-list.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="search-list" v-show="searches.length">
     <transition-group name="list" tag="ul">
-      <li class="search-item" @click="selectItem(item)" v-for="item in searches">
+      <li :key="item" class="search-item" @click="selectItem(item)" v-for="item in searches">
         <span class="text">{{item}}</span>
         <span class="icon" @click.stop="deleteOne(item)">
           <i class="icon-delete"></i>
