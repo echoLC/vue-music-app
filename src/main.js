17ce521b88cd5d467072de1dc6c118fe333dd659
罗超 <luochao@mingbikes.com>
2017-09-21 21:47:52 +0800

    -madd the vconsole

--- src/main.js
+++ src/main.js
@@ -9,6 +9,11 @@ import VueLazyload from 'vue-lazyload'
 
 import 'common/stylus/index.styl'
 
+/* eslint-disable no-unused-vars */
+import vConsole from 'vconsole'
+
+console.log('text')
+
 fastclick.attach(document.body)
 
 Vue.use(VueLazyload, {
