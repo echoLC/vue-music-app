f041bd4317851519d21296ceae588515307eb559
echoLC <651644100@qq.com>
2017-10-20 15:20:57 +0800

    修改了搜索接口的地址

--- src/api/search.js
+++ src/api/search.js
@@ -17,7 +17,7 @@ export function getHotKey() {
 }
 
 export function search(query, page, zhida, perpage) {
-  const url = 'https://c.y.qq.com/soso/fcgi-bin/search_for_qq_cp'
+  const url = 'https://c.y.qq.com/soso/fcgi-bin/client_search_cp'
 
   const data = Object.assign({}, commonParams, {
     w: query,
